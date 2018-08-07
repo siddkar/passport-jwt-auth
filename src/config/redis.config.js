a25dbe51af8b52da96280c113dec0fa66fd21728
avinash14 <avinash.satpathy@outlook.com>
2018-08-07 14:07:28 +0530

    Updated redis.config for redis connect to RedisLabs

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -3,8 +3,18 @@ import bluebird from 'bluebird';
 import { logger } from './pino.config';
 
 bluebird.promisifyAll(redis);
-// connecting to redis with default config
-const redisClient = redis.createClient();
+
+const redisHost = `${process.env.REDIS_HOST}`;
+const redisPort = `${process.env.REDIS_PORT}`;
+const redisKey = `${process.env.REDIS_KEY}`;
+
+
+// connecting to redis with config param
+const redisClient = redis.createClient({
+    port: `${redisPort}`, // replace with your port
+    host: `${redisHost}`, // replace with your hostanme or IP address
+    password: `${redisKey}`, // replace with your password
+});
 
 redisClient.on('connect', () => {
     logger.info({ message: 'Connection with Redis-Server successful!!!' });
