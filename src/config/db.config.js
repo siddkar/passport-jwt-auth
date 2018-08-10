46f72879ea64d832d9e9afa2de575fbbc12b6a2b
avinash14 <avinash.satpathy@outlook.com>
2018-08-10 23:04:25 +0530

    Updated db.config.js & env_template for dbPrefix

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -6,6 +6,7 @@ import { logger } from './pino.config';
 mongoose.Promise = bluebird.Promise;
 
 // Getting the values from env prop files
+const dbPrefix = `${process.env.DB_PREFIX}`;
 const dbName = `${process.env.DB_NAME}`;
 const dbUser = `${process.env.DB_USER}`;
 const dbPass = `${process.env.DB_PASS}`;
@@ -13,7 +14,7 @@ const dbHost = `${process.env.DB_HOST}`; // Includes DBPort
 
 // setting up the uri
 // const uri = `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}`;
-const uri = `mongodb://${dbUser}:${dbPass}@${dbHost}/${dbName}`;
+const uri = `${dbPrefix}://${dbUser}:${dbPass}@${dbHost}/${dbName}`;
 
 /**
  * Mongoose version for > 5.0, mongoose.connecct
